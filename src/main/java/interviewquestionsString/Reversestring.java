cc634caeeece62d3a1380b4fc1e5f1de425193ce
Vanajaramanan <ramananvm999@gmail.com>
2025-04-09 21:53:24 +0530

    rever

--- src/main/java/interviewquestionsString/Reversestring.java
+++ src/main/java/interviewquestionsString/Reversestring.java
@@ -1,7 +1,7 @@
 package interviewquestionsString;
 
 public class Reversestring {
-	
+
 	public static void main(String[] args) {
 		/*
 		 * // here we are going to do string reverse in multiple methods..
@@ -9,11 +9,31 @@ public class Reversestring {
 		 * 
 		 * 1.
 		 */
+
+		String s = " Mathangi";
+
+		String[] rev = s.split("");
+		for (int i = rev.length - 1; i >= 0; i--) {
+
+			System.out.print(rev[i]);
+			System.out.println();
+		}
+//Second method by using stringbuilder methods.
+		StringBuilder sb = new StringBuilder(s);
+		StringBuilder ss = sb.reverse();
+		System.out.println(ss);
 		
-		String s =" Mathangi";
 		
-		
-		
+		// third method using the reverse.
+
+		String chareversed="";
+	 for(int i=s.length()-1; i>=0; i--) {
+		 
+		 chareversed+=	 s.charAt(i);
+	 }
+	 
+	 System.out.println(chareversed);
+
 	}
 
 }
