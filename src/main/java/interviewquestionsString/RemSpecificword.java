86056a065deb0a449a40200948d33e0494d6afde
Vanajaramanan <ramananvm999@gmail.com>
2025-04-10 12:56:52 +0530

    checking

--- src/main/java/interviewquestionsString/RemSpecificword.java
+++ src/main/java/interviewquestionsString/RemSpecificword.java
@@ -2,4 +2,13 @@ package interviewquestionsString;
 
 public class RemSpecificword {
 
+	
+	public static void main(String[] args) {
+		
+		String rem =" I love my india all indians are my brothers and sisters";
+		rem =rem.replace(" I love my india", "");
+		
+		System.out.println(rem);
+	}
+
 }
